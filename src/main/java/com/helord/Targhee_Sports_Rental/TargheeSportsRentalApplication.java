8562c82d4dee559450a06854a6986837284b7494
HSOrduno <103794936+Hsan2022@users.noreply.github.com>
2024-08-04 13:57:04 -0600

    first commit

---
+++ src/main/java/com/helord/Targhee_Sports_Rental/TargheeSportsRentalApplication.java
@@ -0,0 +1,13 @@
+package com.helord.Targhee_Sports_Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargheeSportsRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargheeSportsRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helord/Targhee_Sports_Rental/TargheeSportsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helord.Targhee_Sports_Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargheeSportsRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
